7d136d5851502b207c94cd5ef2a31956bf08bda7
irenacher <irenac@restorationrobotics.com>
2015-08-07 16:00:59 -0700

    Initial commit

---
+++ app/src/main/java/com/example/irenachernyak/androidfragments/FragmentLandscape.java
@@ -0,0 +1,18 @@
+package com.example.irenachernyak.androidfragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by irenachernyak on 8/7/15.
+ */
+public class FragmentLandscape extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.landscape_fragment, container, false);
+    }
+}
